f493ed84ed6aa046b42039b92cd9abe1e4430075
redwolftech <mtoney@redwolftechnologies.com>
2025-05-22 11:03:31 -0400

    Initial commit of old webforms app

---
+++ Wa-Tor/Default.aspx
@@ -0,0 +1,236 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Code Retreat 4/21/2018</title>
+    <link rel="stylesheet" href="css/main.css" />
+    <telerik:RadStyleSheetManager id="RadStyleSheetManager1" runat="server" />
+    <script type="text/javascript">
+        var haveFile = false;
+        var timerid;
+        var haveImages = false;
+        var isPlaying = false;
+        var counter = -1;
+        var timeInt = 200;
+        var doLoop = false;
+        function fileSelected(sender, args) {
+            haveFile = true;
+            document.getElementById('<%#lbUploadFile.ClientID%>').style.backgroundColor = '#645a72';
+            var savemsg = document.getElementById('<%#lblUploadError.ClientID%>');
+            try {
+                savemsg.style.display = 'none';
+            } catch (error) { }
+        }
+        function fileRemoved(sender, args) {
+            haveFile = false;
+            document.getElementById('<%#lbUploadFile.ClientID%>').style.backgroundColor = '#f0f0f0';
+        }
+        function evalButtons() {
+            if (counter > 0) {
+                document.getElementById('<%#lbFirst.ClientID%>').style.backgroundColor = '#645a72';
+                document.getElementById('<%#lbPrev.ClientID%>').style.backgroundColor = '#645a72';
+            } else {
+                document.getElementById('<%#lbFirst.ClientID%>').style.backgroundColor = '#f0f0f0';
+                document.getElementById('<%#lbPrev.ClientID%>').style.backgroundColor = '#f0f0f0';
+            }
+            if (counter == imgArray.length - 1) {
+                document.getElementById('<%#lbNext.ClientID%>').style.backgroundColor = '#f0f0f0';
+                document.getElementById('<%#lbLast.ClientID%>').style.backgroundColor = '#f0f0f0';
+            } else {
+                document.getElementById('<%#lbNext.ClientID%>').style.backgroundColor = '#645a72';
+                document.getElementById('<%#lbLast.ClientID%>').style.backgroundColor = '#645a72';
+            }
+        }
+        function gotImages() {
+            document.getElementById('<%#lbPlay.ClientID%>').style.backgroundColor = '#645a72';
+            evalButtons();
+        }
+        function notImages() {
+            document.getElementById('<%#lbPlay.ClientID%>').style.backgroundColor = '#f0f0f0';
+            document.getElementById('<%#lbStop.ClientID%>').style.backgroundColor = '#f0f0f0';
+            document.getElementById('<%#lbFirst.ClientID%>').style.backgroundColor = '#f0f0f0';
+            document.getElementById('<%#lbPrev.ClientID%>').style.backgroundColor = '#f0f0f0';
+            document.getElementById('<%#lbNext.ClientID%>').style.backgroundColor = '#f0f0f0';
+            document.getElementById('<%#lbLast.ClientID%>').style.backgroundColor = '#f0f0f0';
+        }
+        function addFrame() {
+            counter += 1;
+            if (counter > imgArray.length - 1) {
+                counter = 0;
+            }
+        }
+        function delFrame() {
+            counter -= 1;
+            if (counter < 0) {
+                counter = imgArray.length - 1;
+            }
+        }
+        function doSpdLow() {
+            timeInt = 400;
+        }
+        function doSpdMedium() {
+            timeInt = 200;
+        }
+        function doSpdHigh() {
+            timeInt = 100;
+        }
+        function stopTimer() {
+            try {
+                clearInterval(timerid);
+            } catch (error) { }
+        }
+        function doPlay() {
+            if (haveImages) {
+                if (!isPlaying) {
+                    doLoop = document.getElementById('chkLoop').checked;
+                    isPlaying = true;
+                    document.getElementById('<%#lbPlay.ClientID%>').style.backgroundColor = '#f0f0f0';
+                    document.getElementById('<%#lbFirst.ClientID%>').style.backgroundColor = '#f0f0f0';
+                    document.getElementById('<%#lbPrev.ClientID%>').style.backgroundColor = '#f0f0f0';
+                    document.getElementById('<%#lbNext.ClientID%>').style.backgroundColor = '#f0f0f0';
+                    document.getElementById('<%#lbLast.ClientID%>').style.backgroundColor = '#f0f0f0';
+                    document.getElementById('<%#lbStop.ClientID%>').style.backgroundColor = '#645a72';
+                    document.getElementById('grpSpeed').disabled = true;
+                    timerid = setInterval(
+                        function () {
+                            var doAnimate = true;
+                            if (doLoop) {
+                                addFrame();
+                            }
+                            else {
+                                if (counter < imgArray.length - 1) {
+                                    addFrame();
+                                } else {
+                                    doAnimate = false;
+                                    doStop();
+                                    counter = 0;
+                                }
+                            }
+                            if (doAnimate) {
+                                imgFrame.src = imgArray[counter].src;
+                                document.getElementById('lblFrame').innerText = nameArray[counter];
+                            }
+                        }, timeInt);
+                }
+            }
+        }
+        function doStop() {
+            if (haveImages) {
+                if (isPlaying) {
+                    isPlaying = false;
+                    document.getElementById('<%#lbPlay.ClientID%>').style.backgroundColor = '#645a72';
+                    document.getElementById('<%#lbStop.ClientID%>').style.backgroundColor = '#f0f0f0';
+                    evalButtons();
+                    document.getElementById('grpSpeed').disabled = false;
+                    try {
+                        clearInterval(timerid);
+                    } catch (error) { }
+                }
+            }
+        }
+        function doPrev() {
+            if (haveImages && !isPlaying) {
+                if (counter > 0) {
+                    delFrame();
+                    imgFrame.src = imgArray[counter].src;
+                    document.getElementById('lblFrame').innerText = nameArray[counter];
+                    evalButtons();
+                }
+            }
+        }
+        function doNext() {
+            if (haveImages && !isPlaying) {
+                if (counter < imgArray.length - 1) {
+                    addFrame();
+                    imgFrame.src = imgArray[counter].src;
+                    document.getElementById('lblFrame').innerText = nameArray[counter];
+                    evalButtons();
+                }
+            }
+        }
+        function doFirst() {
+            if (haveImages && !isPlaying) {
+                counter = 0;
+                imgFrame.src = imgArray[counter].src;
+                document.getElementById('lblFrame').innerText = nameArray[counter];
+                evalButtons();
+            }
+        }
+        function doLast() {
+            if (haveImages && !isPlaying) {
+                counter = imgArray.length - 1;
+                imgFrame.src = imgArray[counter].src;
+                document.getElementById('lblFrame').innerText = nameArray[counter];
+                evalButtons();
+            }
+        }
+        function doUpload() {
+            if (haveFile) {
+                doStop();
+                document.getElementById('<%#lblUploadMsg.ClientID%>').innerText = 'Processing File, Please Wait...';
+                haveFile = false;
+                return true;
+            } else {
+                return false;
+            }
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+    <telerik:RadScriptManager ID="RadScriptManager1" runat="server">
+        <Scripts>
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.Core.js" />
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQuery.js" />
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQueryInclude.js" />
+        </Scripts>
+    </telerik:RadScriptManager>
+
+    <telerik:RadAjaxManager ID="RadAjaxManager1" runat="server">
+        <AjaxSettings>
+            <telerik:AjaxSetting AjaxControlID="lbUploadFile">
+                <UpdatedControls>
+                    <telerik:AjaxUpdatedControl ControlID="pnlMain" UpdatePanelCssClass="" />
+                </UpdatedControls>
+            </telerik:AjaxSetting>
+        </AjaxSettings>
+    </telerik:RadAjaxManager>
+
+    <div class="crtitle">Asheville Coder's League - Code Retreat (4/21/2018)</div>
+    <div class="crdesc">WA-TOR Exercise</div>
+
+    <asp:Panel ID="pnlMain" runat="server">
+        <div class="frtitle"><span id="lblFrame">Frame Title:</span></div>
+        <div class="imagecell"><img id="imgFrame" src="template/empty.png" width="450" height="300" border="1" /></div>
+        <div class="speedcell">
+            <fieldset id="grpSpeed" class="radiogroup">
+                <input type="radio" id="spdLow" name="grpSpeed" value="300" onclick="doSpdLow();" /><label for="spdLow">Low</label>
+                <input type="radio" id="spdMedium" name="grpSpeed" value="200" checked="checked" onclick="doSpdMedium();" /><label for="spdMedium">Medium</label>
+                <input type="radio" id="spdHigh" name="grpSpeed" value="100" onclick="doSpdHigh();" /><label for="spdHigh">High</label>&nbsp;&nbsp;
+                &nbsp;&nbsp;<input type="checkbox" id="chkLoop" />Loop Playback
+            </fieldset>
+        </div>
+        <div class="buttonscell">
+            <asp:LinkButton ID="lbPlay" CssClass="disbutton" Width="80" runat="server" Text="Play" OnClientClick="doPlay();return false;"></asp:LinkButton>
+            <asp:LinkButton ID="lbStop" CssClass="disbutton" Width="80" runat="server" Text="Stop" OnClientClick="doStop();return false;"></asp:LinkButton>
+            <asp:LinkButton ID="lbFirst" CssClass="disbutton" Width="80" runat="server" Text="First" OnClientClick="doFirst();return false;"></asp:LinkButton>
+            <asp:LinkButton ID="lbPrev" CssClass="disbutton" Width="80" runat="server" Text="Prev" OnClientClick="doPrev();return false;"></asp:LinkButton>
+            <asp:LinkButton ID="lbNext" CssClass="disbutton" Width="80" runat="server" Text="Next" OnClientClick="doNext();return false;"></asp:LinkButton>
+            <asp:LinkButton ID="lbLast" CssClass="disbutton" Width="80" runat="server" Text="Last" OnClientClick="doLast();return false;"></asp:LinkButton>
+        </div>
+
+        <div class="uploadmsg">Upload Data File:</div>
+        <div class="uploadcell"><telerik:RadAsyncUpload RenderMode="Lightweight" ID="RadAsyncUpload1" TargetFolder="Uploads" runat="server" MaxFileInputsCount="1" OnFileUploaded="RadAsyncUpload1_FileUploaded" OnClientFileSelected="fileSelected" OnClientFileUploadRemoved="fileRemoved"></telerik:RadAsyncUpload></div>
+        <div class="uploadmsg"><asp:Label ID="lblUploadMsg" runat="server" Text="&nbsp;" Visible="true"></asp:Label><asp:Label ID="lblUploadError" runat="server" CssClass="errormsg" Text="Bad Message" Visible="false"></asp:Label></div>
+        <div class="uploadbtn">
+            <asp:LinkButton ID="lbUploadFile" CssClass="disbutton" Width="200" runat="server" Text="Upload and Process" OnClick="lbUploadFile_Click" OnClientClick="return doUpload();"></asp:LinkButton>
+            <asp:LinkButton ID="lbGenerateFile" CssClass="mainbutton" Width="200" runat="server" Text="Generate Random" OnClick="lbUploadFile_Click"></asp:LinkButton>
+        </div>
+    </asp:Panel>
+
+    </form>
+</body>
+</html>
---
+++ Wa-Tor/Default.aspx.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Web.UI;
+using Telerik.Web.UI;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+public partial class Default : System.Web.UI.Page 
+{
+    private List<string> ImageFiles = new List<string>();
+    private List<string> FrameNames = new List<string>();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Handles var include in header
+        Page.Header.DataBind();
+    }
+
+    protected void RadAsyncUpload1_FileUploaded(object sender, FileUploadedEventArgs e)
+    {
+        string FilePath = Server.MapPath("~/Uploads/");
+        string sessionID = this.Session.SessionID + "_";
+        e.File.SaveAs(FilePath + sessionID + e.File.GetName());
+        Session["FileUpload"] = sessionID + e.File.FileName;
+    }
+
+    private bool FormatValid(string format)
+    {
+        string allowableLetters = "0123456789";
+
+        foreach (char c in format)
+        {
+            if (!allowableLetters.Contains(c.ToString()))
+                return false;
+        }
+
+        return true;
+    }
+
+    public static Bitmap ResizeImage(System.Drawing.Image image, int width, int height)
+    {
+        var destRect = new Rectangle(0, 0, width, height);
+        var destImage = new Bitmap(width, height);
+
+        destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+        using (var graphics = Graphics.FromImage(destImage))
+        {
+            graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+            graphics.SmoothingMode = SmoothingMode.None;
+            using (var wrapMode = new ImageAttributes())
+            {
+                wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+            }
+        }
+
+        return destImage;
+    }
+
+    protected void lbUploadFile_Click(object sender, EventArgs e)
+    {
+        string sUploadFile = "";
+        try
+        {
+            sUploadFile = Session["FileUpload"].ToString();
+        }
+        catch { }
+
+        string DefScript = "";
+        DefScript += "var imgArray = new Array();";
+        DefScript += "var nameArray = new Array();";
+        DefScript += "imgFrame.src='template/empty.png';";
+        DefScript += "document.getElementById('lblFrame').innerText='Frame Title:';";
+        DefScript += "notImages();";
+        DefScript += "haveImages=false;";
+        DefScript += "counter=-1;";
+
+        if (sUploadFile != "")
+        {
+            // Check file extension
+            if (Path.GetExtension(sUploadFile).ToUpper() != ".TXT")
+            {
+                // Register script block
+                RadScriptManager.RegisterStartupScript(Page, Page.GetType(), "1", DefScript, true);
+
+                lblUploadMsg.Text = "";
+                lblUploadError.Visible = true;
+                lblUploadError.Text = "Uploaded file should have .txt extension";
+                Session["FileUpload"] = "";
+            }
+            else
+            {
+                try
+                {
+                    // Read in entire text file
+                    string FilePath = Server.MapPath("~/Uploads/");
+                    string SavePath = Server.MapPath("~/images/");
+                    string sessionID = this.Session.SessionID + "_";
+                    string TimeStamp = DateTime.Now.ToString("yyyyMMddHHmmssfff") + "_";
+
+                    string[] readText = File.ReadAllLines(FilePath + sUploadFile);
+
+                    string ErrorMsg = "";
+                    bool ValidFrames = true;
+                    bool StartFrame = true;
+                    int NumLines = 0;
+                    List<string> FrameLines = new List<string>();
+
+                    ImageFiles.Clear();
+                    FrameNames.Clear();
+                    foreach (string sLine in readText)
+                    {
+                        if (ValidFrames)
+                        {
+                            if (StartFrame)
+                            {
+                                // This line is the frame start
+                                FrameNames.Add(sLine);
+                                FrameLines.Clear();
+                                NumLines = 0;
+                                StartFrame = false;
+                            }
+                            else
+                            {
+                                NumLines++;
+                                FrameLines.Add(sLine);
+                                ValidFrames = FormatValid(sLine);
+                                if (ValidFrames)
+                                {
+                                    if (sLine.Length < 75)
+                                    {
+                                        ValidFrames = false;
+                                        ErrorMsg = "Line Too Short in Frame# " + FrameNames.Count.ToString();
+                                    }
+                                    else
+                                    {
+                                        if (sLine.Length > 75)
+                                        {
+                                            ValidFrames = false;
+                                            ErrorMsg = "Line Too Long in Frame# " + FrameNames.Count.ToString();
+                                        }
+                                    }
+                                    if (ValidFrames)
+                                    {
+                                        if (NumLines < 50)
+                                        {
+                                        }
+                                        else
+                                        {
+                                            StartFrame = true;
+
+                                            if (ValidFrames)
+                                            {
+                                                // Ready to make bitmap
+                                                Bitmap myBitmap = new Bitmap(75, 50);
+
+                                                for (int y=0;y < FrameLines.Count;y++)
+                                                {
+                                                    string thisLine = FrameLines[y];
+                                                    for (int x=0;x < thisLine.Length;x++)
+                                                    {
+                                                        string thisChar = thisLine.Substring(x, 1);
+                                                        switch (thisChar)
+                                                        {
+                                                            case "0": // Water
+                                                                myBitmap.SetPixel(x, y, Color.Blue);
+                                                                break;
+                                                            case "1": // Fish
+                                                                myBitmap.SetPixel(x, y, Color.Green);
+                                                                break;
+                                                            case "2": // Sharks
+                                                                myBitmap.SetPixel(x, y, Color.Orange);
+                                                                break;
+                                                            case "3": // Mystery
+                                                                myBitmap.SetPixel(x, y, Color.Purple);
+                                                                break;
+                                                            case "4": // Mystery
+                                                                myBitmap.SetPixel(x, y, Color.Black);
+                                                                break;
+                                                            case "5":
+                                                                myBitmap.SetPixel(x, y, Color.Tan);
+                                                                break;
+                                                            case "6":
+                                                                myBitmap.SetPixel(x, y, Color.Teal);
+                                                                break;
+                                                            case "7":
+                                                                myBitmap.SetPixel(x, y, Color.Red);
+                                                                break;
+                                                            case "8":
+                                                                myBitmap.SetPixel(x, y, Color.Brown);
+                                                                break;
+                                                            case "9":
+                                                                myBitmap.SetPixel(x, y, Color.Pink);
+                                                                break;
+                                                        }
+
+                                                    }
+                                                }
+
+                                                Bitmap myResize = ResizeImage(myBitmap, 450, 300);
+                                                string NewFilename = sessionID + TimeStamp + "frame" + FrameNames.Count.ToString() + ".png";
+                                                myResize.Save(SavePath + NewFilename);
+
+                                                ImageFiles.Add(NewFilename);
+                                            }
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    ErrorMsg = "Invalid Characters in Frame# " + FrameNames.Count.ToString();
+                                }
+                            }
+                        }
+                    }
+                    if (ValidFrames)
+                    {
+                        if (FrameNames.Count != ImageFiles.Count)
+                        {
+                            ValidFrames = false;
+                            ErrorMsg = "Not Enough Lines in Last Frame";
+                        }
+                    }
+
+                    // Image creation
+
+
+                    if (ValidFrames)
+                    {
+                        // Register script block
+                        string ImgScript = "";
+                        ImgScript += "var imgArray = new Array();";
+                        ImgScript += "var nameArray = new Array();";
+                        for (int i = 0; i < FrameNames.Count; i++)
+                        {
+                            ImgScript += "imgArray[" + i.ToString() + "] = new Image();";
+                            ImgScript += "imgArray[" + i.ToString() + "].src = 'images/" + ImageFiles[i] + "';";
+                            ImgScript += "nameArray[" + i.ToString() + "] = '" + FrameNames[i] + "';";
+                        }
+                        ImgScript += "imgFrame.src=imgArray[0].src;";
+                        ImgScript += "document.getElementById('lblFrame').innerText=nameArray[0];";
+                        ImgScript += "gotImages();";
+                        ImgScript += "haveImages=true;";
+                        ImgScript += "counter=0;";
+
+                        RadScriptManager.RegisterStartupScript(Page, Page.GetType(), "1", ImgScript, true);
+
+                        // Update messages
+                        lblUploadMsg.Text = "&nbsp;";
+                        lblUploadError.Visible = false;
+                        Session["FileUpload"] = "";
+                    }
+                    else
+                    {
+                        // Register script block
+                        RadScriptManager.RegisterStartupScript(Page, Page.GetType(), "1", DefScript, true);
+
+                        lblUploadMsg.Text = "";
+                        lblUploadError.Visible = true;
+                        lblUploadError.Text = ErrorMsg;
+                        Session["FileUpload"] = "";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Register script block
+                    RadScriptManager.RegisterStartupScript(Page, Page.GetType(), "1", DefScript, true);
+
+                    lblUploadMsg.Text = "";
+                    lblUploadError.Visible = true;
+                    lblUploadError.Text = "There was an error in processing this file";
+                    Session["FileUpload"] = "";
+                }
+            }
+        }
+        else
+        {
+            // Register script block
+            RadScriptManager.RegisterStartupScript(Page, Page.GetType(), "1", DefScript, true);
+
+            lblUploadMsg.Text = "";
+            lblUploadError.Visible = true;
+            lblUploadError.Text = "You must select a file to upload first";
+            Session["FileUpload"] = "";
+        }
+    }
+}
